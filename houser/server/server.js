ae1cc27c5b25d4cd316f2abd819214e22e9e30c1
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-04-13 11:27:50 -0600

    commit 1

---
+++ houser/server/server.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const bodyParser = require('body-parser')
+const port = 3005
+const app = express()
+const controller = require('./controller')
+const massive = require('massive')
+require('dotenv').config()
+
+app.use(bodyParser.json())
+massive(process.env.connectionString).then(db => app.set('db', db))
+
+
+
+app.listen(port,() => {
+  console.log(`Listening on port ${port}`)
+})
---
+++ houser/src/components/Dashboard/Dashboard.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import House from '../House/House'
+
+export default class Dashboard extends Component {
+  render() {
+    return (
+      <div>
+        <House/>
+      </div>
+    )
+  }
+}
