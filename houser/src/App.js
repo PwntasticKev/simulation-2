8415f6b881a8d8074cf953e0a346741302e8fae4
Kevin Lee <pwntastic@Kevins-MacBook-Pro.local>
2018-04-13 11:48:56 -0600

    meh

--- houser/src/App.js
+++ houser/src/App.js
@@ -4,15 +4,19 @@ import Dashboard from './components/Dashboard/Dashboard'
 import Header from './components/Header/Header'
 import Wizard from './components/Wizard/Wizard'
 import routes from './routes'
+import { Link } from 'react-router-dom'
 
 class App extends Component {
   render() {
-    return (
-      <div className="App">
-       <Header/>
-       {routes}
+    return <div className="App">
+        <Header />
+        {routes}
+        <div>
+         
+            {/* <Link> <button path='/wizard'>Add New Property</button></Link> */}
+          
+        </div>
       </div>
-    );
   }
 }
 
--- houser/src/components/Dashboard/Dashboard.js
+++ houser/src/components/Dashboard/Dashboard.js
@@ -1,12 +1,12 @@
 import React, { Component } from 'react'
 import House from '../House/House'
+// import { Link } from 'react-router-dom'
 
 export default class Dashboard extends Component {
   render() {
-    return (
-      <div>
-        <House/>
+    return <div>
+        <House />
+        
       </div>
-    )
   }
 }
